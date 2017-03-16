8c2558986ac3e2bf02b0e9a6cf54511efafb4c3e
EdyHartono <edyhartono.chen@gmail.com>
2017-03-17 01:54:53 +0700

    change handle message to webhook controller

--- src/main/java/org/transbot/TransBotApplication.java
+++ src/main/java/org/transbot/TransBotApplication.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.transbot.service.MessageConverterService;
 
 
-@LineMessageHandler
+
 @SpringBootApplication
 public class TransBotApplication {
 
@@ -36,40 +36,8 @@ public class TransBotApplication {
 	@Autowired
 	private MessageConverterService messageConverterService;
 
-	private void sendMessage(Event event)
-	{
-		//String to=event.getSource().getUserId();
-		String to="U3df0ca20b02fa0e3583b6b59fe29990e";
-
-		//Messages messages= Messages.MESSAGES;
-		try {
-			String value = objectMapper.writeValueAsString(messageConverterService.convertMessageToRoute(to,"binus-mangga dua mall"));
-			Message message = new TextMessage(value);
-			PushMessage pushMessage = new PushMessage(to, message);
-			lineMessagingClient.pushMessage(pushMessage);
-		}
-		catch (Exception e)
-		{
-			LOG.error(e.getMessage(),e);
-		}
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TransBotApplication.class, args);
-	}
-	
-	@EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-		//kita cari tau dulu solusinya lalu kirim
-		sendMessage(event);
-		System.out.println("event: " + event);
-        return new TextMessage(event.getMessage().getText());
-    }
-	
-	@EventMapping
-	public void handleDefaultMessageEvent(Event event) {
-		//default message
-		sendMessage(event);
-	    System.out.println("event: " + event);
 	}
 }
